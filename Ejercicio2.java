a3ac32ce37e4b0f2f08bc15484c10c8ecda5d399
cgilsanchez <cgilsanchez.02@gmail.com>
2023-10-11 20:33:34 +0200

    first commit

---
+++ Ejercicio2.java
@@ -0,0 +1,7 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Carlos Gil Sanchez");
+        System.out.println("C/ Leopoldo alas clarin");
+        System.out.println("Telefono: 644 74 31 72");
+    }
+}
---
+++ Ejercicio3.java
@@ -0,0 +1,15 @@
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        System.out.printf("%-10s     %-10s\n" ,"computer","ordenador");
+        System.out.printf("%-10s     %-10s\n" ,"student","alumno");
+        System.out.printf("%-10s     %-10s\n" ,"cat","gato");
+        System.out.printf("%-10s     %-10s\n" ,"penguin","pinguino");
+        System.out.printf("%-10s     %-10s\n" ,"machine","maquina");
+        System.out.printf("%-10s     %-10s\n" ,"nature","naturaleza");
+        System.out.printf("%-10s     %-10s\n" ,"light","luz");
+        System.out.printf("%-10s     %-10s\n" ,"green","verde");
+        System.out.printf("%-10s     %-10s\n" ,"book","libro");
+        System.out.printf("%-10s     %-10s\n" ,"pyramid","piramide");
+
+    }
+}
